c3725f572dc45d06c53f98b22d193721382359c8
Anthony Robinson <robinsonamk@gmail.com>
2018-01-23 19:28:20 -0800

    Finished front-end components.

---
+++ lab-anthony/frontend/src/component/auth-redirect/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
+import * as routes from '../../routes';
+
+class AuthRedirect extends React.Component {
+  render() {
+    let {location, history, token} = this.props;
+    let {pathname} = location;
+    let destinationRoute = null;
+
+    if(pathname === routes.LOGIN_ROUTE || pathname === routes.SIGNUP_ROUTE || pathname === routes.ROOT_ROUTE) {
+      if(token) {
+        destinationRoute = routes.DASHBOARD_ROUTE;
+      }
+    }
+    else{
+      if(!token)
+        destinationRoute = routes.ROOT_ROUTE;
+    }
+    return(
+      <div className='auth-redirect'>
+        {destinationRoute ? <Redirect to={destinationRoute} /> : undefined}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  token: state.token,
+});
+
+export default connect(mapStateToProps)(AuthRedirect);
---
+++ lab-anthony/frontend/src/component/dashboard/index.js
@@ -0,0 +1,11 @@
+impor React from 'react';
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div className='dashboard'>
+        <h1>This is the dashboard</h1>
+      </div>
+    )
+  }
+}
