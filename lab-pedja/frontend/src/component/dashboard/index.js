77977c049a89c0cb048235d40ea8b707259828d1
Predrag Josifovic <pjosifovic@yahoo.com>
2018-01-23 19:46:16 -0800

    adding components

---
+++ lab-pedja/frontend/src/component/dashboard/index.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className='dashboard'>
+        <h1>
+          Hello from Dashboard
+        </h1>
+      </div>
+    )
+  }
+}
