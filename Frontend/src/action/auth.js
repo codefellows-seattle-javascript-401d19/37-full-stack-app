1f18a5fb52e676770ae3c1bb1bad457345199ec3
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2018-01-24 00:18:55 -0800

    Committing the rest of tonight's work

--- Frontend/src/action/auth.js
+++ Frontend/src/action/auth.js
@@ -11,7 +11,7 @@ export const removeTokenAction = () => ({
 });
 
 export const signupAction = (user) => (store) => {
-	return superagent.post(`$__API_URL__}${routes.SIGNUP_ROUTE}`)
+	return superagent.post(`$__API_URL__}/${routes.SIGNUP_ROUTE}`)
 	.send(user)
 	.withCredentials()
 	.then(response => {
@@ -21,7 +21,7 @@ export const signupAction = (user) => (store) => {
 };
 
 export const loginAction = (user) => (store) => {
-	return superagent.get(`${__API_URL__}${routes.LOGIN_ROUTE}`)
+	return superagent.get(`${__API_URL__}/${routes.LOGIN_ROUTE}`)
 	.auth(user.username, user.password)
 	.withCredentials()
 	.then(response => {
