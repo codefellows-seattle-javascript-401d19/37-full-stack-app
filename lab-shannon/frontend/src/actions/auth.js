b9508a7fac7a198dcbbe11903610eca0397d91ad
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-23 17:58:09 -0800

    successfully kicking user back to homepage from dashboard if they don't have a token

--- lab-shannon/frontend/src/actions/auth.js
+++ lab-shannon/frontend/src/actions/auth.js
@@ -8,3 +8,13 @@ export const setToken = (token) => ({
 export const removeToken = () => ({
   type: 'REMOVE_TOKEN',
 });
+
+export const signup = (user) => (store) => {
+  return superagent.post(`http://localhost:3000/signup`)
+    .send(user)
+    .withCredentials()
+    .then(response => {
+      console.log(response);
+      // return store.dispatch(setToken(response));
+    });
+};
--- lab-shannon/frontend/src/components/auth-redirect/index.js
+++ lab-shannon/frontend/src/components/auth-redirect/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
 
 class AuthRedirect extends React.Component{
   render(){
-    return <div>All encompassing groot</div>;
-  }
+    let {location, history, token} = this.props;
+    //pathname is the url on the location object
+    let {pathname} = location;
+    let destinationRoute = null;
+
+    if(pathname === '/signup' || pathname === '/login' || pathname === '/'){
+      if(token){
+        destinationRoute = '/dashboard';
+      }
+    }else {
+      if(!token){
+        destinationRoute = '/';
+      }
+    }
+
+    return (
+      <div>
+        <div>testing</div>
+        {destinationRoute ? <Redirect to={destinationRoute} /> : undefined}
+      </div>
+    );}
 }
 
-export default AuthRedirect;
+const mapStateToProps = state => ({
+  token: state.token,
+});
+
+export default connect(mapStateToProps)(AuthRedirect);
