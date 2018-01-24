68c6104fcdd117f4cf991809ef926fccb76f39ae
Cameron Moorehead <hello@cameronmoorehead.com>
2018-01-24 15:08:30 -0800

    updated auth-form

--- lab-cameron/frontend/src/component/app/index.js
+++ lab-cameron/frontend/src/component/app/index.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Landing from '../landing';
 import Dashboard from '../dashboard';
 import AuthRedirect from '../auth-redirect';
 
-const App = () => {
-  return (
-    <div className='app'>
-      <BrowserRouter>
-        <div>
-          <Route path='*' component={ AuthRedirect } />
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/signup' component={Landing} />
-          <Route exact path='/login' component={Landing} />
-          <Route exact path='/dashboard' component={Dashboard} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+import * as clientProfile from '../../action/client-profile';
 
-export default App;
+class App extends Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <BrowserRouter>
+          <div>
+            <Route path='*' component={ AuthRedirect } />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/signup' component={Landing} />
+            <Route exact path='/login' component={Landing} />
+            <Route exact path='/dashboard' component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  loggedIn: !!state.token,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchClientProfile: () => dispatch(clientProfile.fetchAction()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
