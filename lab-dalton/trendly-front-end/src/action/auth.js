0f78f1a74e4d8e068c15cfdc5cf427d962367e01
Dalton Carr <daltonlcarr@yahoo.com>
2018-01-25 10:19:38 -0800

    thought it was working but signup is having a 404 error

--- lab-dalton/trendly-front-end/src/action/auth.js
+++ lab-dalton/trendly-front-end/src/action/auth.js
@@ -1,37 +1,32 @@
 import superagent from 'superagent';
 import * as routes from '../routes';
 
-//----------------------------------------------
-// SYNC
-//----------------------------------------------
+// Sync
+
 export const setTokenAction = (token) => ({
-  type : 'TOKEN_SET',
-  payload : token,
+  type: 'TOKEN_SET',
+  payload: token,
 });
 
 export const removeTokenAction = () => ({
-  type : 'TOKEN_REMOVE',
+  type: 'TOKEN_REMOVE',
 });
 
-//----------------------------------------------
-// ASYNC
-//----------------------------------------------
+// Async
+
 export const signupAction = (user) => (store) => {
   return superagent.post(`${__API_URL__}${routes.SIGNUP_ROUTE}`)
     .send(user)
-    .withCredentials()
     .then(response => {
-      console.log({response});
       return store.dispatch(setTokenAction(response.text));
     });
+
 };
 
 export const loginAction = (user) => (store) => {
   return superagent.get(`${__API_URL__}${routes.LOGIN_ROUTE}`)
-    .auth(user.username,user.password)
-    .withCredentials()
+    .auth(user.username, user.password)
     .then(response => {
-      console.log({response});
       return store.dispatch(setTokenAction(response.text));
     });
 };
--- lab-dalton/trendly-front-end/webpack.config.js
+++ lab-dalton/trendly-front-end/webpack.config.js
@@ -1,38 +1,59 @@
 'use strict';
 
+require('dotenv').config();
+const {DefinePlugin, EnvironmentPlugin} = require('webpack');
+const CleanPlugin = require('clean-webpack-plugin');
+const UglifyPlugin = require('uglifyjs-webpack-plugin');
+
 const HTMLPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const webPackConfig = module.exports = {};
 
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
 webPackConfig.entry = `${__dirname}/src/main.js`;
 webPackConfig.output = {
   filename : 'bundle.[hash].js',
   path : `${__dirname}/build`,
+  publicPath : process.env.CDN_URL,
 };
 
 webPackConfig.plugins = [
-  new HTMLPlugin(),
-  new ExtractTextPlugin('bundle.[hash].css'),
+  new HTMLPlugin({title: 'Full Stack Application! ^-^'}),
+  new EnvironmentPlugin(['NODE_ENV']),
+  new DefinePlugin({
+    __API_URL__ : JSON.stringify(process.env.API_URL),
+    __CDN_URL__ : JSON.stringify(process.env.CDN_URL),
+  }),
+  new ExtractTextPlugin('bundle[hash].css'),
 ];
+
+if(PRODUCTION) {
+  webPackConfig.plugins = webPackConfig.plugins.concat([
+    new UglifyPlugin(),
+    new CleanPlugin(),
+  ]);
+}
 
 webPackConfig.module = {
   rules: [
     {
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel-loader', 
+      loader: 'babel-loader',
     },
     {
       test:  /\.scss$/,
       loader: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
         use: [
-          'css-loader', 
-          'resolve-url-loader', 
+          'css-loader',
+          'resolve-url-loader',
           {
             loader: 'sass-loader',
             options: {
-              sourceMap: true, 
+              sourceMap: true,
               includePaths: [`${__dirname}/src/style`],
             },
           },
@@ -42,7 +63,7 @@ webPackConfig.module = {
   ],
 };
 
-webPackConfig.devtool = 'eval-source-map';
+webPackConfig.devtool = PRODUCTION ? undefined : 'eval-source-map';
 
 webPackConfig.devServer = {
   historyApiFallback: true,
