e93294d35300fbb2a68beabb01115ff41e319b17
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-24 17:28:51 -0800

    fill in profile-form component file

--- lab-shannon/frontend/src/components/profile-form/index.js
+++ lab-shannon/frontend/src/components/profile-form/index.js
@@ -8,12 +8,40 @@ let emptyState = {
 class ProfileForm extends React.Component{
   constructor(props){
     super(props);
-    this.state = emptyState;
+    this.state = this.props.profile ? this.props.profile : emptyState;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event){
+    let {name, value} = event.target;
+    this.setState({[name] : value});
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    this.props.onComplete(this.state);
+    this.setState(emptyState);
+  }
+
+  compontentWillReceiveProps(nextProps){
+    if(nextProps.profile){
+      this.setState(nextProps.profile);
+    }
+  }
+
   render(){
     return (
       <div>
         <h4>Testing out the profile form</h4>
+        <form onSubmit={this.handleSubmit}>
+          <textArea
+            name = 'bio'
+            value = {this.state.bio}
+            onChange={this.handleChange}
+          />
+          <button type='submit'>Submit</button>
+        </form>
       </div>
     );
   }
