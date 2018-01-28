2891e26c88640c3e5c98e53588006e6da258aec5
David Lindahl <davidalindahl@gmail.com>
2018-01-28 13:32:28 -0800

    adding client photos to reducer index and removing meta tag that didnt work from app index

--- lab-david/frontend/src/component/app/index.js
+++ lab-david/frontend/src/component/app/index.js
@@ -26,11 +26,6 @@ class App extends React.Component {
   render(){
     return (
       <div className='app'>
-        
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        />
 
         <BrowserRouter>
           <div>
