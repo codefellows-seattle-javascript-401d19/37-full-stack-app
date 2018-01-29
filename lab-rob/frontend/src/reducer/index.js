3e3229416db4290eec2f263ee82f96b55d26ed69
Robert Reed <robert.mc.reed@gmail.com>
2018-01-28 18:27:25 -0800

    updated reducer/index.js

--- lab-rob/frontend/src/reducer/index.js
+++ lab-rob/frontend/src/reducer/index.js
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux';
 
 import token from './token';
 import profile from './profile';
+import photo from './photo';
 
 export default combineReducers({
   token,
   profile,
+  photo,
 });
