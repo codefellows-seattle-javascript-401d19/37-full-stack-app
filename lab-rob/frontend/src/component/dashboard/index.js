60e87cf460d1d82dc216cbf797daecd7c47af559
Robert Reed <robert.mc.reed@gmail.com>
2018-01-28 18:35:48 -0800

    added photoform to dashboard

--- lab-rob/frontend/src/component/dashboard/index.js
+++ lab-rob/frontend/src/component/dashboard/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+import PhotoForm from '../photo-form';
+import {createPhotoActionRequest} from '../../action/photo';
+
 
 class Dashboard extends React.Component {
   render() {
     return (
       <div className='dashboard'>
         <h2>Sluggram Dashboard</h2>
+        <PhotoForm onComplete={this.props.createPhoto} />
       </div>
     );
   }
 }
 
-export default Dashboard;
+const mapDispatchToProps = dispatch => ({
+  createPhoto: (photo) => dispatch(createPhotoActionRequest(photo)),
+});
+
+export default connect(null, mapDispatchToProps)(Dashboard);
