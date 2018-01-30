e06cdf88a08b63ebc29ccc1f2c1b36c17df5b8b0
David Lindahl <davidalindahl@gmail.com>
2018-01-30 14:45:10 -0800

    deleted corpse code from cookiejs

--- lab-david/frontend/src/lib/cookie.js
+++ lab-david/frontend/src/lib/cookie.js
@@ -1,15 +1,3 @@
-// dump files in here from code base
-
-// let CookieTime = (days) => {
-//   let result = new Date()
-//   result.setTime(result.getTime() + (days * 86400000))
-//   return result.toUTCString()
-// }
-
-// export const CookieCreate = (name, value, days) => {
-//   let expires = days ? ` ${cookieTime(days));` : '
-//   document.cookie = `${name}=${value};${expires} path='/'`
-// }
 
 export const cookieFetchAll = () => {
   return Object.assign(...document.cookie.split(';')
